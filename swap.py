d77ee7ac858bef62cb7ba465790e369f69480c85
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-10 12:26:09 -0800

    Create new file

---
+++ swap.py
@@ -0,0 +1,10 @@
+num1 = int(input("Input a number"))
+num2 = int(input("Input another number"))
+numList = [num1, num2]
+for i in len(numList):
+  print numList[i]
+def swapNum(x, y):
+  numList = [num2, num1]
+  for i in len(numList):
+    print numList[i]
+  
