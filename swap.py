f4cb0a136ea9e04b019e5994773d23ed5157eea3
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-11 15:03:40 -0800

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,7 +1,6 @@
 num1 = int(input("Input a number"))
 num2 = int(input("Input another number"))
 def swapNum(x, y):
-  numList = [num2, num1]
-  for i in range(len(numList)-1):
-    print numList[i]
+  return num2, num1
+print(swapNum(num1,num2))
   
