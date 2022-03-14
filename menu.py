8b069950e10cd1f6d41a561b368dd87ad440a43b
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-13 21:37:12 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,16 +1,32 @@
+"""
+Introduction to Console Programming
+Writing a function to print a menu
+"""
+
+
+# Menu options in print statement
+def print_menu1():
+    print('1 -- America' )
+    print('2 -- Belarus' )
+    print('3 -- China' )
+    print('4 -- Denmark' )
+    print('5 -- Exit')
+    runOptions()
+
+
 # Menu options as a dictionary
-country_options = {
+menu_options = {
     1: 'America',
     2: 'Belarus',
     3: 'China',
     4: 'Denmark',
-    5: 'Estonia',
+    5: 'Exit'
 }
 
 # Print menu options from dictionary key/value pair
 def print_menu2():
-    for key in country_options.keys():
-        print('Country,' key, '--', menu_options[key] )
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key] )
     runOptions()
 
 # menu option 1
@@ -23,12 +39,36 @@ def belarus():
 
 # menu option 3
 def china():
-    print('You chose \' 3 - China\'')
-    
+    print('You chose \'3 - China\'')
+
 # menu option 4
 def denmark():
-    print('You chose \' 4 - Denmark\'')
-    
-# menu option 5
-def estonia():
-    print('You chose \' 5 - Estonia\'')
+    print('You chose \'3 - Denmark\'')
+
+
+# call functions based on input choice
+def runOptions():
+    # infinite loop to accept/process user menu choice
+    while True:
+        try:
+            option = int(input('Enter your choice 1-4: '))
+            if option == 1:
+                america()
+            elif option == 2:
+                belarus()
+            elif option == 3:
+                china()
+            elif option == 4:
+                denmark()
+            # Exit menu    
+            elif option == 5:  
+                print('Exiting! Thank you! Good Bye...')
+                exit() # exit out of the (infinite) while loop
+            else:
+                print('Invalid option. Please enter a number between 1 and 4.')
+        except ValueError:
+            print('Invalid input. Please enter an integer input.')
+
+if __name__=='__main__':
+    # print_menu1()
+    print_menu2()
