e0edd6eacf16a89baef9f795ca621ed0be3552b0
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-17 01:01:02 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,12 @@
-
+import sys 
+def fibonacci_number(n):
+  try: 
+    if n == 0 or n == 1:
+      return n
+    else:
+      return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+  except:
+    sys.exit("No negative numbers! Fool.")
+while True:
+  f = input('Which Fibonacci number to find? (small number): ')
+  print(fibonacci_number(int(f)))
