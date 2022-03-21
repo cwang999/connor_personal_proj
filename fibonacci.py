b0a0c66e367f165c0c6f4efdd4ee5b52eb3885a2
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-20 22:22:23 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,26 @@
-import sys 
-def fibonacci_number(n):
-  try: 
-    if n == 0 or n == 1:
-      return n
-    else:
-      return fibonacci_number(n - 1) + fibonacci_number(n - 2)
-  except:
-    sys.exit("No negative numbers! Fool.")
+def fib_seq(n):
+    if n == 0:
+        print(0)
+        return
+        
+    if n < 0:
+        raise Exception("Can't use negative n")
+    
+    def helper(idx, curr, prev):
+        print(curr)
+        if idx == n:
+            return
+        helper(idx + 1, curr + prev, curr)
+    
+    print(0)
+    helper(1, 1, 0)
+        
+def print_fib(n):
+    try:
+        fib_seq(n)
+    except:
+        print("Invalid n")
+        
 while True:
   f = input('Which Fibonacci number to find? (small number): ')
-  print(fibonacci_number(int(f)))
+  print(fib_seq(int(f)))
